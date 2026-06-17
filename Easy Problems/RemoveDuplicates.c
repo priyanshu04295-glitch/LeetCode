6f7e3fd64899170c6dada6c6325498a59beacca5
Priyanshu Singh <priyanshu04295@gmail.com>
2026-06-17 13:10:06 +0530

    Add files via upload

---
+++ Easy Problems/RemoveDuplicates.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int removeDuplicates(int *nums,int numsSize)
+{
+    if(numsSize==0)
+    {
+        return 0;
+    }
+    int k=1;
+    for(int i=1;i<numsSize;i++)
+    {
+        if(nums[i]!=nums[i-1])
+        {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
+
+int main()
+{
+    int nums[]={0,0,1,1,1,2,2,3,3,3};
+    int numsSize = sizeof(nums)/sizeof(nums[0]);
+    int k = removeDuplicates(nums,numsSize);
+    printf("Number of unique elements : %d\n",k);
+    printf("Elements after removing duplicates : ");
+    for(int i=0;i<k;i++)
+    {
+        printf("%d ",nums[i]);
+    }
+
+}
