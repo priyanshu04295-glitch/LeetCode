e30b7c8f20c161df65f86dc86bb01113cfd0a807
Priyanshu Singh <priyanshu04295@gmail.com>
2026-08-27 11:01:43 +0530

    Add files via upload

---
+++ Leetcode-441.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count=1;
+        int i=-1;
+        while(n>=0)
+        {
+            n = n-count;
+            count++;
+            i++;
+        }
+        return i;
+    }
+};
